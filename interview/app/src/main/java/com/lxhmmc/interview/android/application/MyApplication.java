cd437dc3e7d6d69faa884d22a7b1cc3fc863b1b7
lxh <yc333@qq.com>
2018-03-29 10:24:57 +0800

    create intervier project

---
+++ interview/app/src/main/java/com/lxhmmc/interview/android/application/MyApplication.java
@@ -0,0 +1,16 @@
+package com.lxhmmc.interview.android.application;
+
+import android.app.Application;
+
+/**
+ * Created by lxh on 2018/3/29.
+ */
+
+public class MyApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ interview/app/src/main/java/com/lxhmmc/interview/ui/frame/FrameActivityMain.java
@@ -0,0 +1,15 @@
+package com.lxhmmc.interview.ui.frame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lxhmmc.interview.R;
+
+public class FrameActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_activity_main);
+    }
+}
